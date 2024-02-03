19cd47e928237f331696c0a6f49b21ce3163e61e
momen210 <touristmomen@gmail.com>
2024-02-03 23:02:21 +0600

    completed

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
 // middleware
-var cors = require('cors')
-app.use(cors())
+app.use(cors());
+app.use(express.json())
 
 const users = [
   { id: 1, name: "Kalia", email: "kalia@jalia.com" },
-  { id: 1, name: "Jalia", email: "Jalia@lalia.com" },
-  { id: 1, name: "Vulia", email: "Vulia@valia.com" },
+  { id: 2, name: "Jalia", email: "Jalia@lalia.com" },
+  { id: 3, name: "Vulia", email: "Vulia@valia.com" },
 ];
 
 app.get("/", (req, res) => {
@@ -19,6 +20,15 @@ app.get("/", (req, res) => {
 app.get("/users", (req, res) => {
   res.send(users);
 });
+
+app.post("/users", (req, res) => {
+    console.log("post api hitting");
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    res.send(newUser)
+})
 
 app.listen(port, () => {
   console.log("server is running on", port);
